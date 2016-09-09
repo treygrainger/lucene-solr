affd873d968df1926bca2f7aa537e26b8d441f24
Trey Grainger <code@treygrainger.com>
2016-09-09 01:10:36 -0400

    Fix precommit warnings

--- solr/contrib/semantic-knowledge-graph/src/java/org/apache/solr/search/skg/KnowledgeGraphHandler.java
+++ solr/contrib/semantic-knowledge-graph/src/java/org/apache/solr/search/skg/KnowledgeGraphHandler.java
@@ -102,15 +102,5 @@ public class KnowledgeGraphHandler extends RequestHandlerBase
         return "Ranks tags using corpus of tagged data";
     }
 
-    @Override
-    public String getSource() {
-        return "$Source$";
-    }
-
-    @Override
-    public String getVersion() {
-        return "$Revision$";
-    }
-
 
 }
